ae3ba0ebacd46d70a7f1e1194441e977c35336ab
YJS <yes20230611@outlook.com>
2024-09-07 10:08:37 +0800

    Site updated: 2024-09-07 10:08:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/21/hello-world/","2024/08/21/My-New-Post/","2024/08/27/又要开学了/","2024/08/28/我服了/"];function toRandomPost(){
+var posts=["2024/08/21/My-New-Post/","2024/08/21/hello-world/","2024/08/27/又要开学了/","2024/08/28/我服了/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
